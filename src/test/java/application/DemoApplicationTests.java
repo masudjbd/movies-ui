51ae9e1ac859056b507344dae2f0c46d50fffcb9
Masudur Rahman <masudur.rahman@getzephyr.com>
2017-04-25 12:41:05 -0700

    test

--- src/test/java/application/DemoApplicationTests.java
+++ src/test/java/application/DemoApplicationTests.java
@@ -1,16 +1,9 @@
 package application;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
 public class DemoApplicationTests {
 
 	@Test
