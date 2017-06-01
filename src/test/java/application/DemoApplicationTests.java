10405ba5497198ff112ce3941a7987bfee608324
Masudur Rahman <masudjbd@users.noreply.github.com>
2017-06-01 08:12:16 -0700

    Update DemoApplicationTests.java

--- src/test/java/application/DemoApplicationTests.java
+++ src/test/java/application/DemoApplicationTests.java
@@ -34,4 +34,150 @@ public class DemoApplicationTests {
 		String body = "Como Estas";
 		assertEquals(11,body.length());
 	}
+    
+    @Test
+	public void exampleTest5() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+     @Test
+	public void exampleTest6() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest7() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest8() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest9() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest10() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest11() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest12() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest13() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest14() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest15() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+    
+      @Test
+	public void exampleTest16() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest17() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest18() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest19() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest20() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest21() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest22() {
+		String body = "Cosmo Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest23() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest24() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+    
+      @Test
+	public void exampleTest25() {
+		String body = "Comoee Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest26() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest27() {
+		String body = "Como Estas";
+		assertEquals(10,body.length());
+	}
+    
+      @Test
+	public void exampleTest28() {
+		String body = "Como3 Estas";
+		assertEquals(10,body.length());
+	}
 }
